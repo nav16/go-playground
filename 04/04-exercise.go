385a4b6f87b6e31197132c40b4e9e2c6c6545245
Navneet Pandey <npandey057@gmail.com>
2019-09-29 20:27:55 +0530

    Add 04 exercise

---
+++ 04/04-exercise.go
@@ -0,0 +1,26 @@
+package main
+
+// Hands-on exercise #4
+// ● FYI - nice documentation and new terminology “underlying type” ○ https://golang.org/ref/spec#Types
+// For this exercise
+// 	1. Create your own type. Have the underlying type be an int.
+// 	2. create a VARIABLE of your new TYPE with the IDENTIFIER “x” using the “VAR”
+// 			keyword
+// 	3. in func main
+// 		a. print out the value of the variable “x”
+// 		b. print out the type of the variable “x”
+// 		c. assign 42 to the VARIABLE “x” using the “=” OPERATOR
+// 		d. print out the value of the variable “x”
+
+import "fmt"
+
+type ola int
+
+var x ola
+
+func main() {
+	fmt.Printf("%v\n", x)
+	fmt.Printf("%T\n", x)
+	x = 42
+	fmt.Println(x)
+}
