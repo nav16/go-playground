4d547909fc3433ce67c7f05422d1f65eb1fe24b5
Navneet Pandey <npandey057@gmail.com>
2019-09-29 20:06:58 +0530

    Add 01 exercise

---
+++ 01-exercise.go
@@ -0,0 +1,24 @@
+package main
+
+// exercise #1
+// 1. Using the short declaration operator, ASSIGN these VALUES to VARIABLES with the IDENTIFIERS “x” and “y” and “z”
+//  a. 42
+//  b. “James Bond”
+//  c. true
+// 2. Now print the values stored in those variables using
+//  a. a single print statement
+//  b. multiple print statements
+
+import "fmt"
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println(x, y, z)
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
